c76599eea883491e9c114f5b024f9110d3f2245b
ilitoS <79296030+ilitoS@users.noreply.github.com>
2022-02-01 21:54:37 -0500

    initial changes

---
+++ BookCatalogWebAPI/Controllers/BookCatalogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BookCatalogWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookCatalogController
+    {
+        private readonly ILogger<BookCatalogController> _logger;
+
+        public BookCatalogController(ILogger<BookCatalogController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
